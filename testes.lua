163c0b74a79d13061b62ea2afc140720160ced15
nt <netzwii@gmail.com>
2026-05-15 17:19:28 -0300

    Update testes.lua

--- testes.lua
+++ testes.lua
@@ -143,7 +143,7 @@ local lastFlickTime = 0
 local isWallHopping = false
 local lastWallHopTime = 0
 local WALLHOP_GRACE_TIME = 1.5
-local WALLHOP_COOLDOWN = 0.25
+local WALLHOP_COOLDOWN = 0.35
 
 local canDoubleJump = false
 local lastDoubleJump = 0
@@ -2145,7 +2145,6 @@ local function setupCharacter(char)
 		if new == Enum.HumanoidStateType.Landed then
 			canDoubleJump = false
 			lastHitPosition = nil
-			lastFlickTime = 0
 			airborneSource = nil
 			airborneStartY = nil
 			airborneStartTime = 0
@@ -3526,4 +3525,4 @@ createModeSelector(function(mode)
 	applyVisibility()
 end)
 
-print("Best Flee The Facility | Made by Nyhito - Loaded Ssssuccessfully ✅")
+print("Best Flee The Facility | Made by Nyhito - Loaded Successfully ✅")
